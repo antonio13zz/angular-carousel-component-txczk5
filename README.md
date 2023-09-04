981972a01680ee7eafd75f69d56304bd412ca2a6
antonio13zz <144022805+antonio13zz@users.noreply.github.com>
2023-09-04 13:50:24 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-carousel-component-txczk5
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-carousel-component-cnctca)
---
+++ app/app.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: [ './app.component.css' ]
+})
+export class AppComponent  {
+   items = [
+    { title: 'Slide 1' },
+    { title: 'Slide 2' },
+    
+    { title: 'Slide 3' },
+  ]
+
+  addSlide() {
+    const test = 'test';
+    this.items.push({
+      title: `Slide 4`
+    });
+  }
+
+}
